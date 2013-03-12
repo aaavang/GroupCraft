0a8319cb0318f949d35a4f837beae6d254fdd428
2052967a <alex.aavang@gmail.com>
2013-03-12 19:05:01 +0000

    tag cloud fix

--- groupcraft/css.py
+++ groupcraft/css.py
@@ -4,7 +4,10 @@ def  css_processor(request):
 	CSS = os.environ['CSS']
 
 	try:
-		return {'CSS': CSS}
+		if CSS == 'zurb':
+			return {'CSS': CSS, 'NIGHT':False}
+		else:
+			return {'CSS': CSS, 'NIGHT':True}
 	except NameError:
 		CSS = 'zurb'
-		return {'CSS': CSS}
+		return {'CSS': CSS, 'NIGHT':False}
